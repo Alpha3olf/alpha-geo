f001e7d35f90611e399edf75086cc1ec265386a6
Monius <m1105930979@gmail.com>
2023-08-11 14:26:58 +0800

    add dev branch

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,46 @@
-use rayon::prelude::*;
-use std::path::Path;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 use std::fs;
-use regex::Regex;
-use chrono::Local;
+use std::io::Read;
 
-#[derive(Debug)]
-struct Coordata{
-    name: String,
-    raw: String,
-    _content: Option<String>,
-    _toml: Option<String>,
-    _csv: Option<String>,
-}
-
-impl Coordata{
-    fn new(n:&str, c: &str) -> Coordata{
-        Coordata{
-            name:n.to_string(), 
-            raw: c.to_string(), 
-            _content: None, 
-            _toml: None, 
-            _csv: None
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    if req.method() == hyper::Method::GET{
+        if req.uri().path() == "/data.zip" {
+            let mut file = fs::File::open("data.zip").unwrap();
+            let mut contents: Vec<u8> = Vec::new();
+            file.read_to_end(&mut contents).unwrap();
+            let response = Response::builder()
+                .header("Content-Type", "application/octet-stream")
+                .body(Body::from(contents))
+                .unwrap();
+            Ok(response)
         }
-    }
+        else {
+            Ok(Response::new("Hello, World".into()))
+        }
 
-    fn from_file(file: &fs::DirEntry) -> Coordata{
-        let _name = file.file_name();
-        let _content = fs::read_to_string(file.path()).unwrap();
-        Coordata::new(&_name.to_string_lossy(), &_content)
-    }
-
-    fn trim(mut self) -> Coordata{
-        let re = Regex::new(r"(?m)^\s*\n").unwrap();
-        let _content = re.replace_all(&self.raw,"");
-        self._content = Some(_content.to_string());
-        self
-    }
-
-    fn save(self, dir: &str){
-        let _path = format!("{}/Finished_{}", dir, self.name);
-        let _ = fs::write(_path, self._content.unwrap());
+    } else {
+        let response = Response::builder()
+            .status(404)
+            .body(Body::from("Not Found"))
+            .unwrap();
+        Ok(response)
     }
 }
 
-trait CoordFile {
-    fn is_txt(&self) -> bool;
-}
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-impl CoordFile for fs::DirEntry {
-    fn is_txt(&self) -> bool {
-        self.path().extension() == Some("txt".as_ref())
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle_request) )
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
-}
-
-fn main() ->  Result<(), std::io::Error>{
-    let date = Local::now();
-    let input_dir = "./input";
-    let _output_dir = "./output";
-    let output_dir: &str = &format!("{}/{}", _output_dir, date.format("%Y%m%d%H%M").to_string());
-    fs::create_dir_all(input_dir)?;
-    
-    let _input_dir = Path::new(input_dir);
-
-    let _coordata: Vec<_> = fs::read_dir(input_dir)?
-        .filter_map(Result::ok)
-        .par_bridge()
-        .filter(|f| f.is_txt())
-        .map(|d| Coordata::from_file(&d))
-        .collect();
-
-    fs::create_dir_all(output_dir)?;
-    let _ = _coordata.into_iter()
-        .par_bridge()
-        .for_each(|d| d.trim().save(output_dir));
-    Ok(())
 }
